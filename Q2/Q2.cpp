6904448dfcd9d0bf72a6f47b276fce1092e65728
GuvuYO <125232767+gvuyo@users.noreply.github.com>
2023-03-12 13:07:59 +0800

    Add files via upload

---
+++ Q2/Q2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int a;
+    float k = 0;
+    cin >> a;
+    k = a * 1.6;
+    cout << fixed << setprecision(1) << k << endl;
+}
