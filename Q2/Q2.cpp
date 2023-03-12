55a364f9ff65f6496da415023b4ce8a64d02f2b3
GuvuYo <yoyo241357@gmail.com>
2023-03-12 13:35:00 +0800

    Q4賴昱華

--- Q2/Q2.cpp
+++ Q2/Q2.cpp
@@ -1,22 +1,45 @@
-﻿#include<iostream>  
-using namespace std;  
-   
-int main(){  
-    int n=0;  
-    int bin[8];  
-    cin>>n;  
-    if (n<0)  
-    {  
-        n+=256; //負數則推移至正整數  
-    }  
-    for (int i = 7; i >= 0; i--)  
-    {  
-        bin[i]=n%2;  
-        n/=2;  
-    }  
-    for (int i = 0; i < 8; i++)  
-    {  
-        cout<<bin[i];  
-    }  
-    cout<<"\n";  
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int start_hour, start_minute, end_hour, end_minute;
+    int duration_hour, duration_minute;
+    int total_fee = 0;
+
+    /*輸入開始時間*/
+    cout << "";
+    cin >> start_hour >> start_minute;
+
+    /* 輸入結束時間*/
+    cout << "";
+    cin >> end_hour >> end_minute;
+
+    /*計算停車時間（以半小時為單位）*/ 
+    duration_hour = end_hour - start_hour;
+    duration_minute = end_minute - start_minute;
+    if (duration_minute < 0) {
+        duration_hour--;
+        duration_minute += 60;
+    }
+    int duration_half_hour = ceil((duration_hour * 60 + duration_minute) / 30.0);
+
+    /*根據費率計算停車費*/ 
+    if (duration_half_hour <= 4) {
+        if (duration_half_hour <= 4) {
+            total_fee = duration_half_hour * 30;
+        }
+        else {
+            total_fee = 2 * 30 + (duration_half_hour - 2) * 40;
+        }
+    }
+    else {
+        total_fee = 2 * 30 + 2 * 40 + (duration_half_hour - 4) * 60;
+    }
+
+    // 輸出停車費
+    cout << "停車費為：" << total_fee << " 元\n";
+
+    return 0;
 }
