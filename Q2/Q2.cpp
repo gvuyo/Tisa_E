06885200877cddce2731b754ca4bfd5ac610eab8
GuvuYo <yoyo241357@gmail.com>
2023-03-12 13:20:06 +0800

    Q

---
+++ Q2/Q2.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    int m, n;
+    while (cin >> m >> n) {
+        int R = 10000;
+        double iR = (m * m) + (n * n);
+        if (iR < R) {
+            cout << "inside\n";
+        }
+        else {
+            cout << "outside\n";
+        }
+    }
+    return 0;
+}
