7729097df2d456cf3fb05f153937b3d79efa05f6
GuvuYo <yoyo241357@gmail.com>
2023-03-12 13:39:41 +0800

    Q3張柏藝

--- Q2/Q2.cpp
+++ Q2/Q2.cpp
@@ -1,17 +1,16 @@
-﻿#include<iostream>
-using namespace std;
-int main()
-{
-    int m,n;
-    while(cin>>m>>n){
-        int R=10000;
-        double iR=(m*m)+(n*n);
-        if(iR<R){
-            cout<<"inside\n";
-        }
-        else{
-            cout<<"outside\n";
-        }
-    }
-    return 0;
-}
+﻿#include <iostream>    
+using namespace std;    
+int main()    
+{    
+    int x, y;    
+    int r = 100;    
+    int x1 = 0, y1 = 0;      
+    cin >> x >> y;    
+    if ((x - x1) * (x - x1) + (y - y1) * (y - y1) <= r * r)    
+        cout << "inside" << endl;    
+    else    
+        cout << "outside" << endl;    
+    //cout << x << " " << y << " " << r << " " << endl;    
+    
+    return 0;    
+}  
